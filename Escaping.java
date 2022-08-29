a2821e15fb61c99e8ab0e44b5698c6cec1b17045
eyanez0711 <111064506+eyanez0711@users.noreply.github.com>
2022-08-29 00:42:15 -0400

    Update Escaping.java

--- Escaping.java
+++ Escaping.java
@@ -1,2 +1,7 @@
 public Class Escaping{
 public static void main(String[] args) {
+System.out.println("//////////////////////");
+System.out.println("||||| Eric Yanez |||||"");
+System.out.println("\\\\\\\\\\\\\\\\\\\\\\");
+}
+}

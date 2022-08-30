60fac22c4af49fc329179e9af72f86799a23d97d
eyanez0711 <111064506+eyanez0711@users.noreply.github.com>
2022-08-30 00:42:40 -0400

    Update Escaping.java

--- Escaping.java
+++ Escaping.java
@@ -1,3 +1,8 @@
+//Class Escaping.java
+//Author Eric Yanez
+//@version 1.0
+//Course: ITEC 2140 Fall 2022
+//Written: August 27, 2022
 public Class Escaping{
 public static void main(String[] args) {
 System.out.println("//////////////////////");

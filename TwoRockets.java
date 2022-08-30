075913f936d4f91839492a511700a0a37e3abe20
eyanez0711 <111064506+eyanez0711@users.noreply.github.com>
2022-08-30 00:35:36 -0400

    Update TwoRockets.java

--- TwoRockets.java
+++ TwoRockets.java
@@ -1,3 +1,30 @@
 public class TwoRockets{
-  public static void main(String[] args){
+    public static void main(String[] args) {
+        top_bottom();
+        centerBox();
+        unitedStates();
+        centerBox();
+        top_bottom();
+    }
+    static void top_bottom(){
+        print(" /\\\t" + " /\\");
+        print(" / \\\t"+"/ \\");
+        print(" / \\\t"+" / \\");
+    }
+    static void centerBox(){
+        print("+------+"+"+------+");
+        print("||"+"||");
+        print("||"+"||");
+        print("+------+"+"+------+");
+    }
+    static void unitedStates(){
+        print("|United|"+"|United|");
+        print("|States|"+"|States|");
+    }
+    static void print(String string){
+        System.out.println(string);
+
+    }
+}
+
     
